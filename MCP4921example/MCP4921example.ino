7ea83017a51707922907bc7b6ad77a50cdf0bd31
Timothy Roche <kc2vsa@gmail.com>
2019-04-10 22:25:58 -0400

    Working example of the 2 dacs

---
+++ MCP4921example/MCP4921example.ino
@@ -0,0 +1,29 @@
+#include <SPI.h>
+#include <MCP492X.h>
+
+#define PIN_SPI_CHIP_SELECT_DAC_TOP 10 // Or any pin you'd like to use
+#define PIN_SPI_CHIP_SELECT_DAC_BOT 9 // Or any pin you'd like to use
+
+MCP492X myDacTop(PIN_SPI_CHIP_SELECT_DAC_TOP);
+MCP492X myDacBot(PIN_SPI_CHIP_SELECT_DAC_BOT);
+
+void setup() {
+  // put your setup code here, to run once:
+  myDacTop.begin();
+  myDacBot.begin();
+}
+
+void loop() {
+  bool odd = false;
+  bool buffered = true;
+  bool gain = true;
+  bool active = true;
+  unsigned int value_top = 4000; 
+  unsigned int value_bottom = 2048; 
+  // put your main code here, to run repeatedly:
+
+  // Write any value from 0-4095
+  //myDac.analogWrite(2048);
+  myDacTop.analogWrite(odd, buffered, gain, active, value_top);
+  myDacBot.analogWrite(odd, buffered, gain, active, value_bottom);
+}
